291ff2e76e267126c1cc500331241731fc45e296
Object_417 <belovedofthelord1@gmail.com>
2023-05-04 16:27:04 +0100

    added a section to home screen and footer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,19 +4,25 @@ import "@fortawesome/fontawesome-free/js/brands"
 import "@fortawesome/fontawesome-free/js/solid"
 import "@fortawesome/fontawesome-free/js/fontawesome"
 
-import "bootstrap/dist/js/bootstrap"
+import { Tooltip } from "bootstrap/dist/js/bootstrap"
 
 import Swiper, { Navigation, Pagination } from "swiper"
 
-import "swiper/css/bundle"
-
+// Disable form submit
 const $searchForm = document.getElementById("search-form")
 $searchForm.onsubmit = (e) => false
 
+// Initialize Bootstrap tooltips
+const $tooltips = document.querySelectorAll("[data-bs-toggle=tooltip]")
+for (const $tooltip of $tooltips) {
+  const tooltip = new Tooltip($tooltip, { boundary: document.body })
+}
+
+// Initialize Swiper slider
 const coursesSlider = new Swiper(".courses-slider", {
   modules: [Navigation],
   direction: "horizontal",
-  // loop: true,
+  loop: true,
   navigation: {
     prevEl: ".swiper-button-prev",
     nextEl: ".swiper-button-next",
@@ -26,12 +32,14 @@ const coursesSlider = new Swiper(".courses-slider", {
   spaceBetween: 16,
   breakpoints: {
     576: {
+      loop: false,
       slidesPerView: 2,
       navigation: {
         enabled: true
       }
     },
     992: {
+      loop: false,
       slidesPerView: 3,
       navigation: {
         enabled: true
