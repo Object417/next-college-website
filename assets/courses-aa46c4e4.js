a8cc071b5d662b967230ccd58a157d7c5ada95e1
Object_417 <belovedofthelord1@gmail.com>
2023-05-11 11:07:24 +0100

    Updates

--- assets/courses-aa46c4e4.js
+++
@@ -1,1 +0,0 @@
-import"./main-23b294b8.js";console.info("courses page script loaded");const e=document.getElementById("course-search-form");e.onsubmit=o=>!1;
---
+++ assets/courses-cb213cf6.js
@@ -0,0 +1,1 @@
+import"./main-f7df1b2b.js";console.info("courses page script loaded");const e=document.getElementById("course-search-form");e.onsubmit=o=>!1;
