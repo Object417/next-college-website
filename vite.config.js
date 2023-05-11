a8afe03373990f9d781463441d099ee5ca6f6100
Object_417 <belovedofthelord1@gmail.com>
2023-05-11 09:05:23 +0100

    removed unused packages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
+import path, { resolve } from "path"
 import { defineConfig } from "vite"
 import jsconfigPaths from "vite-jsconfig-paths"
-import path, { resolve } from "path"
-import { fileURLToPath } from "url"
-import { createHtmlPlugin } from "vite-plugin-html"
 
 export default defineConfig({
   base: "/next-college-website/",
@@ -21,16 +19,6 @@ export default defineConfig({
         courses: resolve(__dirname, "src/pages/courses.html"),
         "contact-us": resolve(__dirname, "src/pages/contact-us.html"),
         "art-course": resolve(__dirname, "src/pages/art-course.html")
-        // index: fileURLToPath(new URL("index.html", import.meta.url)),
-        // courses: fileURLToPath(
-        //   new URL("public/pages/courses.html", import.meta.url)
-        // ),
-        // "art-course": fileURLToPath(
-        //   new URL("public/pages/art-course.html", import.meta.url)
-        // ),
-        // "contact-us": fileURLToPath(
-        //   new URL("public/pages/contact-us.html", import.meta.url)
-        // )
       }
     }
   }
